a9722af88cabb75bdcacf42b1ef3aa0564ce5981
saschakiefer <mail@sascha-kiefer.de>
2022-07-11 18:24:40 +0000

    zettelkasten 0.1.2

--- Formula/zettelkasten.rb
+++ Formula/zettelkasten.rb
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.0.0 at 2022-07-11T18:24:34.931668Z
+# Generated with JReleaser 1.0.0 at 2022-07-11T18:24:38.653208Z
 class Zl < Formula
   desc "My Zettelkasten interpretation"
-  homepage "https://github.com/saschakiefer/zettelkasten/framework"
+  homepage "https://github.com/saschakiefer/zettelkasten/bootstrap"
   url "https://github.com/saschakiefer/zettelkasten/releases/download/0.1.2/zettelkasten-0.1.2-osx-x86_64.zip"
   version "0.1.2"
   sha256 "ad609f50e24eb8ccdb3084d236aff73b0cb326bf8797c266f3408027cdd2567f"
