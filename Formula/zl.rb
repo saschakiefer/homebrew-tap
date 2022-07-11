29a864e5bee4e04d9272b5785bf518cd6d59c5d7
saschakiefer <mail@sascha-kiefer.de>
2022-07-11 20:58:53 +0000

    zl 0.1.3

--- Formula/zl.rb
+++ Formula/zl.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.0.0 at 2022-07-11T18:24:38.653208Z
+# Generated with JReleaser 1.0.0 at 2022-07-11T20:58:46.80436Z
 class Zl < Formula
   desc "My Zettelkasten interpretation"
-  homepage "https://github.com/saschakiefer/zettelkasten/bootstrap"
-  url "https://github.com/saschakiefer/zettelkasten/releases/download/0.1.2/zettelkasten-0.1.2-osx-x86_64.zip"
-  version "0.1.2"
-  sha256 "ad609f50e24eb8ccdb3084d236aff73b0cb326bf8797c266f3408027cdd2567f"
+  homepage "https://github.com/saschakiefer/zettelkasten"
+  url "https://github.com/saschakiefer/zettelkasten/releases/download/0.1.3/zettelkasten-0.1.3-osx-x86_64.zip"
+  version "0.1.3"
+  sha256 "7eff7c9cc98de6fa5d655c1ee73743ed501c4906673f262e77627b3312bb715d"
   license "Apache-2.0"
 
 
@@ -15,6 +15,6 @@ class Zl < Formula
 
   test do
     output = shell_output("#{bin}/zl --version")
-    assert_match "0.1.2", output
+    assert_match "0.1.3", output
   end
 end
